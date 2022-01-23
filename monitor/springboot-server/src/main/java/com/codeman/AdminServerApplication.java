5879c3239d2034a9124bcad343c8cf896b2fdae9
CodeLogic <53108370+hdgaadd@users.noreply.github.com>
2022-01-23 17:52:16 +0800

    monitor

---
+++ monitor/springboot-server/src/main/java/com/codeman/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.codeman;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAdminServer
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+}
