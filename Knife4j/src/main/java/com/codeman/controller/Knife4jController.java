942adb683d16b655d699a606a8e86f77463e4a40
CodeLogic <53108370+hdgaadd@users.noreply.github.com>
2022-03-16 22:01:10 +0800

    Test Knife4j

---
+++ Knife4j/src/main/java/com/codeman/controller/Knife4jController.java
@@ -0,0 +1,25 @@
+package com.codeman.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hdgaadd
+ * Created on 2022/03/16
+ */
+@Api(tags = "Knife4j-test")
+@RestController
+public class Knife4jController { // http://localhost:8080/doc.html
+
+    @ApiImplicitParam(name = "name", value = "姓名", required = true) // 参数描述
+    @ApiOperation(value = "方法描述")
+    @RequestMapping("/Knife4j-test")
+    public String test(@RequestParam(value = "name") String name) {  // 该value中的数据代表url传递的参数名称，如https://localhost/test/?name=name
+        return name + " say:" + "hello, Knife4j";
+    }
+
+}
