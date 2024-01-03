d2315f446d96176f02305baf2f9a0e6be56cf47e
hdgaadd <53108370+hdgaadd@users.noreply.github.com>
2024-01-03 23:55:00 +0800

    feat(middleware): add WebFlux

---
+++ middleware/WebFlux/src/main/java/org/codeman/WebFlux/WebFluxApplication.java
@@ -0,0 +1,13 @@
+package org.codeman.WebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxApplication.class, args);
+	}
+
+}
---
+++ middleware/WebFlux/src/test/java/org/codeman/WebFlux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeman.WebFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
